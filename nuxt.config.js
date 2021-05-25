e1cdc4509965fa1d8d20b83861f94eece13d4bac
prashanth.rv <prashanth.rv@zohocorp.com>
2021-05-25 15:46:44 +0530

    Package name change

--- nuxt.config.js
+++ nuxt.config.js
@@ -10,11 +10,14 @@ export default {
   // Global page headers: https://go.nuxtjs.dev/config-head
   head: {
     title: 'TN Covid Beds',
-    titleTemplate: '%s',
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-      { hid: 'description', name: 'description', content: '' },
+      {
+        hid: 'description',
+        name: 'description',
+        content: 'Find beds near you',
+      },
     ],
     link: [
       { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' },
